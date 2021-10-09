32d438156b2346b3fa7006278cc523ada597424b
eric <iseric.chenyj@gmail.com>
2021-10-10 05:05:34 +0800

    write get ASL_women label

--- detectron2/tool/get_ASL_women_label.py
+++ detectron2/tool/get_ASL_women_label.py
@@ -3,10 +3,7 @@ import os
 from pathlib import Path
 import argparse
 import pandas as pd
-'''
-    function:
-        output label file: video_id  video_label
-'''
+
 
 def get_trans_dict(transfromFile):
     '''
@@ -26,13 +23,6 @@ def get_trans_dict(transfromFile):
     return trans_dict
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('transformFile',type=str, help='path/to/transform_txt')
-    args = parser.parse_args()
-    return args
-
-
 def get_id_label_pair(dataset_info, transform_table):
     '''
         input:
@@ -50,8 +40,41 @@ def get_id_label_pair(dataset_info, transform_table):
     return labels
 
 
+def get_key(val, dict):
+    for key, value in dict.items():
+        if val == value:
+            return key
+    return "key doesn't exist"
+
+
+def get_women_transformDict(trans_dict, rename_dict):
+    '''
+        input:
+            trans_dict
+                dict: <ori_id, temp_id>
+            rename_dict
+                dict: <temp_id, final_id>
+        output:
+            out_dict
+                dict: <ori_id, current_id>
+    '''
+    out_dict = {}
+    for key, val in sorted(rename_dict.items()):
+        ori_id = get_key(key, trans_dict)
+        out_dict[str(ori_id)] = str(val)
+    return out_dict
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('videoTransform',type=str, help='path/to/videoTransform_txt')
+    parser.add_argument('rename_txt',type=str, help='path/to/rename_txt')
+    args = parser.parse_args()
+    return args
+
+
 def dump2txt(outPath, id_label_pair):
-    f = open(outPath, 'w') 
+    f = open(outPath, 'w')
     for video_id in id_label_pair.keys():
         f.write(video_id+'\t'+id_label_pair[video_id]+'\n')
     f.close()
@@ -59,13 +82,17 @@ def dump2txt(outPath, id_label_pair):
 
 def main():
     args = get_args()
-    outPath = os.path.join('/home/Datasets/ASL/To_111/', str(args.transformFile).split("/")[-2], 'test_label.txt')
+    outPath = os.path.join('/home/Datasets/ASL/To_111/', str(args.videoTransform).split("/")[-2], 'test_label.txt')
     dataset_info = pd.read_csv(open('/home/Datasets/ASL_information.csv', 'r'))
     dataset_info = dataset_info[['video_id','gloss']]
     # transformation info
-    trans_dict = get_trans_dict(transfromFile= open(args.transformFile, 'r'))
-    id_label_pair = get_id_label_pair(dataset_info, transform_table=trans_dict)
+    trans_dict = get_trans_dict(transfromFile= open(args.videoTransform, 'r'))
+    rename_dict = get_trans_dict(transfromFile= open(args.rename_txt, 'r'))
+    _women_trans_dict = get_women_transformDict(trans_dict, rename_dict)
+
+    id_label_pair = get_id_label_pair(dataset_info, transform_table = _women_trans_dict)
     dump2txt(outPath, id_label_pair)
+
 
 if __name__ == '__main__':
     main()
